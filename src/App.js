469be57bbb727f2ab4a2746257fedbe67d3cdd59
N-Musa <n.musa@gmx.net>
2021-11-21 13:53:19 +0100

    initial commit :octocat:

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, eligendi!
+    </div>
+  )
+}
+
+export default App
+
