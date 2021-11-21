5af0d82ce2934761c323d495b6ba2a216de001eb
N-Musa <n.musa@gmx.net>
2021-11-21 13:55:51 +0100

    tailwindcss config 🛸

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-  purge: [],
+  purge: ['./components/**/*.js', './pages/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["'Poppins', sans-serif"],
+        Quicksand: ["'Quicksand', sans-serif"]
+      },
+      colors: {
+        Primary_bg: "#edede9",
+        Secondary_bg: "#FDFCF8",
+        Orange: "#E15638",
+        Black: "#24242c",
+        Text: "#fefaec",
+
+      }
+
+    },
   },
   variants: {
     extend: {},
