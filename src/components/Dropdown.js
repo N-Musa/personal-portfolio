891c713488f8da8ebcb560a38c5308907b0bd314
N-Musa <n.musa@gmx.net>
2021-11-23 19:38:26 +0100

    Main components directory

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { BsTwitter } from 'react-icons/bs';
 
-const Dropdown = () => {
+
+const Dropdown = ({ isOpen, toggle }) => {
     return (
-        <div>
-            
+
+        <div className={isOpen
+            ? ' flex justify-center flex-col items-center bg-Orange text-white space-y-5 p-5 cursor-pointer'
+            : 'hidden'}
+            onClick={toggle}>
+
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Hire me</Link>
+
+            <div className='flex space-x-8 text-xl p-2'>
+                <a href="https://github.com/N-Musa?tab=repositories" target='_blank' rel='noopener noreferrer'>
+                    <AiFillGithub />
+                </a>
+                <a href="https://twitter.com/iMusaO9" target='_blank' rel='noopener noreferrer'>
+                    <BsTwitter />
+                </a>
+                <a href="#" target='_blank' rel='noopener noreferrer'>
+                    <AiFillLinkedin />
+                </a>
+            </div>
         </div>
+
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import {BrowserRouter as Router,} from "react-router-dom";
-import { Route, Link } from 'react-router-dom'
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { BsTwitter } from 'react-icons/bs';
+import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi';
 
-const Navbar = () => {
+const Navbar = ( { toggle }) => {
     return(
-        <>
-        <nav>
-            <div className="text-xl text-blue-600">Nuradin.Musa</div>
+
+        <nav className='container py-6 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 shadow-sm font-Poppins'>
+            <Link to='/' className='text-2xl font-bold'>Nuradin.Musa</Link>
+            <div className="hidden md:flex space-x-10 items-center">
+            <Link to="/">Home</Link>
+            <Link to="/projects" className='px-6 py-2 rounded-md font-bold bg-Orange text-white'>Projects
+           
+            </Link>
+            <Link to="/contact">Hire me
+            <span className="animate-ping absolute inline-flex rounded-full h-3 w-3 bg-Orange"></span>
+            </Link>
+            </div>
+            <div onClick={toggle}
+                className="md:hidden cursor-pointer">
+                <GiHamburgerMenu className='text-3xl' />
+            </div>
         </nav>
-        </>
     )
 }
 
