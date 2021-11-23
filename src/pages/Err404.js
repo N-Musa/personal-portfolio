764ecd0290c68cc7de55543e4ecdca2a66e19aed
N-Musa <n.musa@gmx.net>
2021-11-23 20:54:15 +0100

    Added Content to 404 Page

--- src/pages/Err404.js
+++ src/pages/Err404.js
@@ -1,7 +1,11 @@
 const Err404 = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div className="flex justify-center items-center mt-32 text-xl font-bold pl-1">
+           <div className="space-y-2">
+            <h1>404</h1>
+            <hr /> 
+            <h1>This page could not be found.</h1>
+            </div>
         </div>
     )
 }
