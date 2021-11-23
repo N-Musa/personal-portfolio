5a17a822bb853c936802002efa4188b45e411266
N-Musa <n.musa@gmx.net>
2021-11-23 19:41:13 +0100

    Added Hooks && Routes to pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
+import { Routes, Route} from "react-router-dom";
+import Dropdown from './components/Dropdown';
 import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Err404 from './pages/Err404';
 
 const App = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () =>{
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if(window.innerWidth > 768 && isOpen){
+        setIsOpen(false)
+      }
+    }
+    window.addEventListener('resize', hideMenu)
+
+    return () =>{
+      window.removeEventListener('resize', hideMenu)
+    }
+  })
+
   return (
     <>
-    <Navbar />
+
+    <Navbar toggle={toggle}/>
+    <Dropdown isOpen={isOpen} toggle={toggle}/>
+    <Routes> 
+      <Route path="/" element={<Home />} /> 
+      <Route path="/projects" element={<Projects />} /> 
+      <Route path="/contact" element={<Contact />} /> 
+      <Route path="*" element={<Err404 />} /> 
+    </Routes>
+    
     </>
+
   )
 }
 
