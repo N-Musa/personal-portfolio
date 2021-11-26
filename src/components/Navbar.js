a1da54846e0e5db4838f7dcfa27b59c3470b055d
N-Musa <n.musa@gmx.net>
2021-11-26 15:48:12 +0100

    modify nav links styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = ( { toggle }) => {
         <nav className='container py-6 flex justify-between items-center mx-auto px-8 md:px-14 lg:px-24 shadow-sm font-Poppins'>
             <Link to='/' className='text-2xl font-bold'>Nuradin.Musa</Link>
             <div className="hidden md:flex space-x-10 items-center">
-            <Link to="/">Home</Link>
-            <Link to="/projects" className='px-6 py-2 rounded-md font-bold bg-Orange text-white'>Projects
+            <Link to="/" className='nav-links'>Home</Link>
+            <Link to="/projects" className='nav-links'>Projects
            
             </Link>
-            <Link to="/contact">Hire me
+            <Link to="/contact" className='nav-links'>Hire me
             <span className="animate-ping absolute inline-flex rounded-full h-3 w-3 bg-Orange"></span>
             </Link>
             </div>
