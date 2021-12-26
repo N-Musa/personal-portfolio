be4ed5d56643ce4d00607ceb430d8769ec50372e
N-Musa <n.musa@gmx.net>
2021-12-26 16:55:58 +0100

    fix form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,7 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_USER_ID)
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, process.env.REACT_APP_USER_ID,  form.current)
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
